d40dd5471ae67bbb2c47d8cc61a9a9dd08695e67
Marcella Mendoza <99848492+MarcellaMendoza@users.noreply.github.com>
2022-05-17 02:59:41 +0000

    Initial commit

---
+++ practice.js
@@ -0,0 +1,91 @@
+// //Practice 1
+// let air = 32
+// let message = ""
+
+// if (air < 50){
+//   console.log("You can't breathe outside right now. Danger!") 
+// } else {
+//   console.log("It's safe to breathe.")
+// }
+
+// let age = 30
+// let ageMessage = ""
+
+// if (age < 30) {
+//   console.log("Cool")
+// } else {
+//   console.log("Old geezer!")
+// }
+
+// let pickles = 145
+// let pickleAnswer = ""
+
+// if (pickles <= 100) {
+//   console.log("I love pickles!")
+// } else {
+//   console.log("Still love pickles!")
+// }
+
+// let hoursCoding = 878
+// let codingAnswer = ""
+
+// if (hoursCoding <= 80) {
+//   console.log("Never enough!")
+// } else {
+  
+//   console.log("Too much maybe..")
+// }
+
+// let weeks = 2
+// let answer = ""
+
+// if (weeks < 12) {
+//   console.log("Hang in there!")
+// } else {
+//   console.log("You're a web developer!")
+// }
+
+
+//Practice 2
+// let age = 29
+// let answer = ""
+
+// if (age < 30) {
+//   console.log("Live another day.")
+// } else if (age === 29) {
+//   console.log("Your crystal is flashing. You have one year left.")
+// } else if (age === 30) {
+//   console.log("It's time for the Carousel!")
+//   } else {
+//   console.log("The Sandmen are chasing you.")
+// }
+
+// let pointsToWin = 47
+// let answer = ""
+
+// if (pointsToWin <= 10) {
+//   console.log("You lose!")
+// } else if (pointsToWin <= 20) {
+//   console.log("No win!")
+// } else if (pointsToWin <= 30) {
+//   console.log("Almost!") 
+// } else {
+//   console.log("You won!") 
+// }
+
+// let appleAday = 365
+// let appleAnswer = ""
+
+// if(appleAday < 365) {
+//   console.log("Go the doctor!")
+// } else {
+//   console.log("Kept the doctor away!")
+// }
+
+// console.log(4 === 3) // false
+// console.log(5 > 2) // true
+// console.log(12 > 12) // false
+// console.log(3< 0) // false
+// console.log(3 >= 3) // true
+// console.log(11 <= 11) // true
+// console.log(3 <= 2) // false
---
+++ script.js
@@ -0,0 +1,48 @@
+// // //Practice 1
+// let oxygen = 50
+// let responseMessage = ""
+
+//   if (oxygen < 50) {
+//     responseMessage = "You can't breathe outside right now. DANGER!"
+//   } else {
+//     responseMessage = "It's safe to breathe."
+//   }
+//   console.log(responseMessage)
+
+
+//Practice 2
+// let age = 30
+// let responseMessage = ""
+
+//   if (age < 30) {
+//     responseMessage = "Live another day."
+//   } else if (age === 29) {
+//     responseMessage = "Your crystal is flashing. You have one year left."
+//   } else if ( age === 30 ) {
+//     responseMessage = "It's time for the Carrousel!"
+//   } else {
+//     responseMessage = "The Sandmen are chasing you."
+//   }
+//   console.log(responseMessage)
+
+//Practice 3
+// console.log(4 === 3)  // false 
+// console.log(5 > 2)    // true
+// console.log(12 > 12)  // false
+// console.log(3 < 0)    // false
+// console.log(3 >= 3)   // true
+// console.log(11 <= 11) // true
+// console.log(3 <= 2)   // false
+
+/*
+  if (age <29){
+
+  } else if (age == 29){
+
+  } else if (age === 30){
+
+  } else { // age>30
+
+  }
+
+*/
